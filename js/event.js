43aed9ee34e0a0eaffa1619ad7218d91b3dee9ca
llj72 <llj72@naver.com>
2021-06-17 10:05:02 +0900

    add

--- js/event.js
+++ js/event.js
@@ -1,11 +1,17 @@
 //상세 설명 보기 클릭 - 텍스트 보이기
 function showText(){
-    var text=document.getElementById("desc");
-    text.style.display="block";
+    //var text = document.getElementById("desc");
+    //text.style.display = "block"; //수직으로 보잉기
+    document.getElementById("desc").style.display="block";
+
+    document.getElementById("btn").style.display="none";
 }
 
 //상세 설명 닫기 클릭 - 텍스트 숨기기
 function hideText(){
     var text=document.getElementById("desc");
-    text.style.display="none";
+    text.style.display="none"; //텍스트 숨기기
+    
+    var button=document.getElementById("btn");
+    button.style.display="block";
 }
